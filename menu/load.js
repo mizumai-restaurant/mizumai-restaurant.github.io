c51a4d8fb05b8a1e89668423458249675d3f9c45
mizumai-restaurant <mizu.mai.neuss@gmail.com>
2025-06-16 12:40:15 +0200

    change url

--- menu/load.js
+++ menu/load.js
@@ -3,12 +3,12 @@
   if (!container) return;
 
   try {
-    const res = await fetch('https://mizumai-restaurant.github.io/menu/menu.html');
+    const res = await fetch('https://pages.mizumai.de/menu/menu.html');
     container.innerHTML = await res.text();
 
     const link = document.createElement('link');
     link.rel = 'stylesheet';
-    link.href = 'https://mizumai-restaurant.github.io/menu/menu.css';
+    link.href = 'https://pages.mizumai.de/menu/menu.css';
     document.head.appendChild(link);
   } catch (err) {
     console.error('Failed to load menu:', err);
